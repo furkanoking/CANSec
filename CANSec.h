bf6c8e5bbd87bee9ece67f27aef978f1a3f21272
furkanozen <furkanoozen4@gmail.com>
2025-11-18 16:36:32 +0000

    danglin pointer issue has been solved

--- CANSec.h
+++ CANSec.h
@@ -3,6 +3,7 @@
 #include <functional>
 #include <array>
 #include <span>
+#include <mutex>
 
 class CANSec {
 public:
@@ -12,18 +13,36 @@ public:
 
     void sendMessage(const int iID, const bool bIsEncrypted, const unsigned char* cpData, const int iDataLength);
     void setReceivedEvent(std::function<void()> ReceivedFunction);
+
+    /**
+     * @brief set the key of the ECUs
+     * @param key The key to set
+     * @return true if the key was set successfully, false otherwise
+     */
+    void setKey(const std::array<__uint8_t,32> &key);
+
+    /**
+     * @brief get the key of the ECUs
+     * @return the current key
+     */
+    std::array<__uint8_t,32> getKey() ;
+
+    void EncryptMessage(const std::span<__uint8_t> plaintext, const int& plaintext_len, std::span<__uint8_t> ciphertext, int& ciphertext_len,std::span<__uint8_t> tag) const;
+
+    void DecryptMessage(const std::span<__uint8_t> ciphertext, const int& ciphertext_len, std::span<__uint8_t> plaintext, int& plaintext_len, const std::span<__uint8_t> expected_tag) const;
+
 private:
+    /**
+     * @brief Mutex to protect access to the key
+     */
+    std::mutex m_mutexKey{};
 
     std::function<void()> m_funcReceivedFunction; // Callback function for received messages
     void receiveMessage() const ;
     int m_iCounter{0};
-    void EncryptMessage(const std::span<__uint8_t> plaintext, const int& plaintext_len, std::span<__uint8_t> ciphertext, int& ciphertext_len,std::span<__uint8_t> tag) const;
     void NonceValueGenerator();
     std::array<__uint8_t,12> m_arrNonceValue{};
     std::array<__uint8_t,32> m_arrKey{};
-
-    void DecryptMessage(const std::span<__uint8_t> ciphertext, const int& ciphertext_len, std::span<__uint8_t> plaintext, int& plaintext_len, const std::span<__uint8_t> expected_tag) const;
-
 
 };
 #endif //CANSECKOD_LIBRARY_H
